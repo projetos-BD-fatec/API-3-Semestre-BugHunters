1dc0719f7143a21c4f82641559634497682f759d
André Soares Junqueira <andresoaresjunqueira14@gmail.com>
2026-09-22 20:35:09 -0300

    [SCRUM-9] feat: cria tabela de contatos do prestador

--- docker/init/01_schema.sh
+++ docker/init/01_schema.sh
@@ -4,6 +4,7 @@ set -e
 sqlplus -s -l "fatec_user/${APP_USER_PASSWORD}@localhost:1521/FREEPDB1" <<'EOF'
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 @/sql/V1__prestador_usuario_perfil.sql
+@/sql/prestador_contato.sql
 @/sql/pre_guia.sql
 @/sql/carga_completa.sql
 EXIT
